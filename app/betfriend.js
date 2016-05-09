e15ca12735bb100e34a41729ca6cda5942aedde2
jon-wade <jonwadeuk@gmail.com>
2016-05-09 08:07:13 +0100

    complete homepage, pulling data for prediction section

--- app/betfriend.js
+++ app/betfriend.js
@@ -32,7 +32,11 @@ app.config(function($routeProvider) {
             controller: 'constructorCtrl'
         })
         .otherwise('/')});
-app.controller('homeCtrl', ['$scope', '$location', 'ergast', '$interval', function($scope, $location, ergast, $interval){
+app.controller('homeCtrl', ['$scope', '$location', 'ergast', '$interval', '$rootScope', function($scope, $location, ergast, $interval, $rootScope){
+
+    //switch off navigation
+    $rootScope.bckHide = true;
+    $rootScope.fwdHide = true;
 
     //methods for clicking through to the next page
     $scope.getPredictionPage = function(){
@@ -58,6 +62,7 @@ app.controller('homeCtrl', ['$scope', '$location', 'ergast', '$interval', functi
                 $scope.raceName = raceScheduleArr[i].raceName;
                 $scope.raceDate = new Date(raceScheduleArr[i].date).toDateString();
                 $scope.circuitName = raceScheduleArr[i].Circuit.circuitName;
+                $rootScope.circuitId = raceScheduleArr[i].Circuit.circuitId;
                 $scope.round = raceScheduleArr[i].round;
                 currentRaceIndex = i;
                 break;
@@ -93,7 +98,100 @@ app.controller('homeCtrl', ['$scope', '$location', 'ergast', '$interval', functi
 
 
 }]);
-app.controller('predictionCtrl', ['$scope', function($scope){
+app.controller('predictionCtrl', ['$scope', 'ergast', '$rootScope', '$q', function($scope, ergast, $rootScope, $q){
+
+    //switch on the back button
+    $rootScope.bckHide = false;
+    $rootScope.fwdHide = true;
+    $scope.circuitHistory = [];
+    var counter = 0;
+
+    //get data for this controller from the ergast API
+    $scope.getData = function() {
+        console.log('GETTING DATA');
+        return $q(function (resolve, reject) {
+            //initially need a list of all drivers in the next grand prix
+            //TODO: the driver list for the current race is based on the previous race's drivers - currently hardcoded!!!!
+            ergast.callAPI('http://ergast.com/api/f1/2016/4/drivers.json').then(function (response) {
+                console.log(response);
+                $scope.driverArr = response.data.MRData.DriverTable.Drivers;
+                console.log($scope.driverArr);
+                //get each drivers history at current circuit
+                for (var i = 0; i < $scope.driverArr.length; i++) {
+                    //then we need each driver's historic performance at that track
+                    ergast.callAPI('http://ergast.com/api/f1/circuits/' + $rootScope.circuitId + '/drivers/' + $scope.driverArr[i].driverId + '/results.json')
+                        .then(function (response) {
+                            //console.log(response);
+                            $scope.circuitHistory[counter] = response.data.MRData.RaceTable.Races;
+                            //console.log($rootScope.circuitHistory);
+                            counter++;
+                            if (counter==$scope.driverArr.length){
+                                counter=0;
+                                resolve();
+                            }
+                        }, function (error) {
+                            console.log(error);
+                            reject();
+                        });
+                }
+            }, function (error) {
+            });
+        });
+    };
+
+    $scope.getData().then(function(){
+        console.log('ALL DATA RETURNED', $scope.circuitHistory);
+        //work out score for each driver based on historic track record at circuit
+
+        //first create a driver data storage object from the current driverArr
+        $scope.driverDataObj = {};
+        for (var x=0; x<$scope.driverArr.length; x++){
+            $scope.driverDataObj[$scope.driverArr[x].driverId] = $scope.driverArr[x];
+        }
+        //append the historic track performance to the object
+
+        for (var i=0; i<$scope.circuitHistory.length; i++){
+
+            //some drivers don't have any history at the track, so check for empty arrays
+            if ($scope.circuitHistory[i].length != 0){
+
+                //console.log($scope.circuitHistory[i][0].Results[0].Driver.driverId);
+                //in the driverDataObj, by driverID, add a new property, trackHistory, equal to the data
+                //returned for that driverId through the call to the ergastAPI (the $scope.circuitHistory object)
+                $scope.driverDataObj[$scope.circuitHistory[i][0].Results[0].Driver.driverId].trackHistory = $scope.circuitHistory[i];
+            }
+        }
+
+        //print out the historic race results for each driver in the race
+
+        for (var driver in $scope.driverDataObj){
+            console.log(driver);
+            if($scope.driverDataObj[driver].trackHistory == undefined){
+                $scope.driverDataObj[driver].trackHistory = [];
+            }
+            else {
+                for (var k = 0; k < $scope.driverDataObj[driver].trackHistory.length; k++) {
+                    console.log('Season: ', $scope.driverDataObj[driver].trackHistory[k].season);
+                    console.log('Position: ', $scope.driverDataObj[driver].trackHistory[k].Results[0].position);
+                    //console.log(driver, $scope.driverDataObj[driver].trackHistory[k].season);
+                }
+            }
+        }
+
+
+        console.log($scope.driverDataObj);
+
+
+
+    }, function(){});
+
+
+
+
+
+
+
+
 
 
 
